6ea469032e97fb6b11d79601da284574b1a1988b
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-05 10:52:54 -0400

    basic api functionality

--- movie-api.js
+++ movie-api.js
@@ -1,21 +1,47 @@
 // movie api 
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '797577513fmsh416afd2e0137296p1dee25jsn6deba288b462',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
-};
+let genreForm = document.querySelector('form')
+let genreInput = document.getElementById('genreInput')
 
-fetch(`https://moviesdatabase.p.rapidapi.com/titles?genre=Action&list=top_boxoffice_200&sort=year.decr&limit=50`, options)
-	.then(response => response.json())
-	.then(response => {
-		console.log(response.results[0])})
-	.catch(err => console.error(err));
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '797577513fmsh416afd2e0137296p1dee25jsn6deba288b462',
+// 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+// 	}
+// };
+
+// fetch(`https://moviesdatabase.p.rapidapi.com/titles?genre=Action&list=top_boxoffice_200&sort=year.decr&limit=50`, options)
+// 	.then(response => response.json())
+// 	.then(response => {
+// 		console.log(response.results[0])})
+// 	.catch(err => console.error(err));
 
 	// getting movies by genre
 
-function displayMovieOptions(x){
-	
-}
+
+	genreForm.addEventListener('submit', (event)=>{
+		event.preventDefault()
+		let genre = genreInput.value
+		console.log(genre)
+		displayMovieOptions(genre)
+	})
+
+	function displayMovieOptions(x){
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '797577513fmsh416afd2e0137296p1dee25jsn6deba288b462',
+				'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+			}
+		};
+		
+		fetch(`https://moviesdatabase.p.rapidapi.com/titles?genre=${x}&list=top_boxoffice_200&sort=year.decr&limit=50`, options)
+			.then(response => response.json())
+			.then(response => {
+				console.log(response.results[0])})
+			.catch(err => console.error(err));
+		
+	}
+// displayMovieOptions()
+
